c77091b8679522648737978baf633d87c067f01c
Mary Castorani <mgcastorani@gmail.com>
2024-09-02 20:20:36 -0600

    server routing and some jazz

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Storefront from './Storefront.jsx'
+
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
-      <Route index element={<Shop/>}/>
+      {/* <Route index element={<App/>}/> */}
+      <Route path='/shop' element={<Storefront/>}/>
     </Route>
   )
 )
@@ -16,6 +20,7 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+
   </StrictMode>,
 )
