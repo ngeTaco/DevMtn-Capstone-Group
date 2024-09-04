fbec95e09be4c51bf52ed49c2da179f679d9877d
Aldo <thealdo.531@gmail.com>
2024-09-03 20:46:10 -0600

    User seed script portion done

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,11 +4,32 @@ import db from '../server/config/db.js'
 
 // Import JSONs for seed data
 import userData from './Data/users.json' assert { type: 'json' };
-import shopperData from './Data/shoppers.json' assert { type: 'json' };
+import historyData from './Data/shoppers.json' assert { type: 'json' };
 import inventoryData from './Data/inventory.json' assert { type: 'json' };
+import { password } from 'pg/lib/defaults';
 
 console.log('Syncing database...');
 await db.sync({ force: true });
 
 console.log('Seeding database...');
 
+
+// seeding Users
+const usersInDB = await Promise.all(
+    userData.map((user) => {
+        const { isAdmin, userName, password, firstName, lastName, points } = user;
+
+        const newUser = Location.create({
+            isAdmin: isAdmin,
+            userName: userName,
+            password: password,
+            firstName: firstName,
+            lastName: lastName,
+            points: points
+        });
+
+        return newUser;
+    }),
+);
+
+console.log('Users seeded successfully!', usersInDB);
