d0de3be4a60ad9c14687a65f7a2632dbd3df2b46
prestonm44 <preston.mitchell4444@gmail.com>
2024-09-05 21:33:29 -0600

    Lacking the rows

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -4,30 +4,25 @@ function ProfilePage() {
 
     return (
         <div class="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
-            <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+            <div class="container bg-white  shadow-lg    transform   duration-200 easy-in-out">
                 <div class=" h-32 overflow-hidden" >
-                    <img class="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+                    <img class="w-full" src="https://images.unsplash.com/photo-1621568670868-24a7dfc590e9?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
                 </div>
-                <div class="flex justify-center px-5  -mt-12">
-                    <img class="h-32 w-32 bg-white p-2 rounded-full   " src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+                <div class="flex justify-center px-5  ">
+                    <img class="h-60 w-60 bg-white p-2 mt-16 mb-8 rounded-full   " src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
 
                 </div>
                 <div class=" ">
-                    <div class="text-center px-14">
-                        <h2 class="text-gray-800 text-3xl font-bold">Mohit Dhiman</h2>
-                        <a class="text-gray-400 mt-2 hover:text-blue-500" href="https://www.instagram.com/immohitdhiman/" target="BLANK()">@immohitdhiman</a>
-                        <p class="mt-2 text-gray-500 text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+                    <div class="text-center mt-4 px-14">
+                        <h2 class="text-gray-800 text-3xl mb-4 font-bold">Preston Mitchell</h2>
+                        <a class="text-gray-400 mt-2 hover:text-blue-500" href="https://www.instagram.com/immohitdhiman/" target="BLANK()">@username</a>
+                        <p class="mt-4 text-gray-500 text-sm">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
                     </div>
-                    <hr class="mt-6" />
+                    <hr class="mt-14" />
                     <div class="flex  bg-gray-50 ">
-                        <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
-                            <p><span class="font-semibold">2.5 k </span> Followers</p>
+                        <div class="text-center w-full p-4 hover:bg-gray-100 cursor-pointer">
+                            <p><span class="font-semibold">250 </span> Points</p>
                         </div>
-                        <div class="border"></div>
-                        <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
-                            <p> <span class="font-semibold">2.0 k </span> Following</p>
-                        </div>
-
                     </div>
                 </div>
             </div>
