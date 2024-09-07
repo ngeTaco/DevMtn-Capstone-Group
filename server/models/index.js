74bfcd905ad04643cd245b3a436322c374cd6e84
Aldo <thealdo.531@gmail.com>
2024-09-07 15:08:32 -0600

    Fixed middle table associations and changed seed order to facilitate those relationships

--- server/models/index.js
+++ server/models/index.js
@@ -3,17 +3,8 @@ import { User } from "./user.model.js";
 import { Inventory } from "./inventory.model.js";
 import { History } from "./history.model.js";
 
-// Define Associations using History as Middle Table
-User.belongsToMany(Inventory, {
-    through: 'histories',
-    foreignKey: { name: 'itemId', field: 'item_id' }
-});
-Inventory.belongsToMany(User, {
-    through: 'histories',
-    foreignKey: { name: 'userId', field: 'user_id' }
-});
 
-// Define Associations to History Table
+// Define Middle Table Associations to History Table
 User.hasMany(History, { foreignKey: 'userId' });
 History.belongsTo(User, { foreignKey: 'userId' });
 
