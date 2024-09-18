491543ddb094db6becea9decec15741950876a4c
prestonm44 <preston.mitchell4444@gmail.com>
2024-09-17 21:14:53 -0600

    conditional rendering no longer changes width of table

--- src/components/AdminInventoryPage/AdminInventoryRow.jsx
+++ src/components/AdminInventoryPage/AdminInventoryRow.jsx
@@ -9,27 +9,27 @@ function AdminInventoryRow() {
         <tr className="hover:bg-gray-50">
             <th className="px-6 py-4 text-sm text-gray-500">1</th>
             {isEditing ?
-                <td className="px-6 py-4 text-sm text-gray-500"><input placeholder="Name" /></td>
+                <td className="px-6 py-4 text-sm text-gray-500"><input className="max-w-14" placeholder="Name" /></td>
                 :
                 <td className="px-6 py-4 text-sm text-gray-500">Charizard</td>
             }
             {isEditing ?
                 <td className="px-6 py-4 text-sm text-gray-500"><input placeholder="Description" /></td>
                 :
-                <td className="px-6 py-4 text-sm text-gray-500">Extremely fierce and basically an auto-win</td>
+                <td className="px-6 py-4 text-sm text-gray-500">Extremely fierce and basically an auto-win.</td>
             }
             {isEditing ?
-                <td className="px-6 py-4 text-sm text-gray-500"><input placeholder="Price" /></td>
+                <td className="px-6 py-4 text-sm text-gray-500"><input className="max-w-10" placeholder="Price" /></td>
                 :
                 <td className="px-6 py-4 text-sm text-gray-500">500 points</td>
             }
             {isEditing ?
-                <td className="px-6 py-4 text-sm text-gray-500"><input placeholder="Quantity" /></td>
+                <td className="px-6 py-4 text-sm text-gray-500"><input className="max-w-16" placeholder="Quantity" /></td>
                 :
                 <td className="px-6 py-4 text-sm text-gray-500">1</td>
             }
             {isEditing ?
-            <td className="px-6 py-4 text-sm text-gray-500"><input type="checkbox" /></td>
+            <td className="px-6 py-4 text-sm text-gray-500"><input className="max-w-3" type="checkbox" /></td>
             :
             <td className="px-6 py-4 text-sm text-gray-500">False</td>
         }
