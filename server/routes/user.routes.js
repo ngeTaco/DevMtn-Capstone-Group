48b5b8510d500e040423e875172e1f6013599af1
Aldo <thealdo.531@gmail.com>
2024-09-19 20:08:29 -0600

    create new user and edit user endpoints created and tested

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -12,4 +12,30 @@ userRouter.get('/:userId', async (req, res) => {
     res.json(user);
 })
 
+// PUT update user by userId
+userRouter.put('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const { isAdmin, userName, password, firstName, lastName, points, profileImage } = req.body;
+    const userChange = await User.findByPk(userId);
+
+    userChange.isAdmin = isAdmin;
+    userChange.userName = userName;
+    userChange.password = password;
+    userChange.firstName = firstName;
+    userChange.lastName = lastName;
+    userChange.points = points;
+    userChange.profileImage = profileImage;
+    
+    await userChange.save();
+    res.json(userChange);
+})
+
+// POST create new user requiring admin state, username, password, first and last name, and initial points amount
+userRouter.post('/new', async (req, res) => {
+    const { isAdmin, userName, password, firstName, lastName, points, profileImage } = req.body;
+    const newUser = await User.create({ isAdmin, userName, password, firstName, lastName, points, profileImage });
+
+    res.json(newUser);
+})
+
 export default userRouter;
