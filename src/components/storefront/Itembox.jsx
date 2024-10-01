0653dbd5657fcbdec92cc17b528231b772ba3239
Aldo <thealdo.531@gmail.com>
2024-09-30 20:56:47 -0600

    formatting clean up and console logs removed

--- src/components/storefront/Itembox.jsx
+++ src/components/storefront/Itembox.jsx
@@ -7,49 +7,44 @@ import { useDispatch } from "react-redux"
 function Itembox(props) {
     const dispatch = useDispatch()
 
-    function openModal () {
-        console.log('clicked')
+    function openModal() {
         dispatch({
-            type: `HANDLE_MODAL` ,
+            type: `HANDLE_MODAL`,
             payload: true
-        }) 
+        })
     }
-    console.log("inventoryprops", props)
+
     return (
-
-        
-            <div className="group block overflow-hidden border space-x-5 border-gray-400 rounded-md">
-                <a role="button" onClick={openModal} className="block overflow-hidden">
-                    <img
-                        src={props.imageUrl}
-                        alt=""
-                        className="h-[250px] object-contain w-full rounded-t-md bg-slate-200 " />
-
-                    <div className="relative bg-white p-3">
-                        <div className="flex justify-between items-center">
-                            <h3 className="text-lg">
-                                <ItemName
+        <div className="group block overflow-hidden border space-x-5 border-gray-400 rounded-md">
+            <a role="button" onClick={openModal} className="block overflow-hidden">
+                <img
+                    src={props.imageUrl}
+                    alt=""
+                    className="h-[250px] object-contain w-full rounded-t-md bg-slate-200 "
+                />
+                <div className="relative bg-white p-3">
+                    <div className="flex justify-between items-center">
+                        <h3 className="text-lg">
+                            <ItemName
                                 itemName={props.itemName}
-                                
-                                />
-                            </h3>
-                            <span className="text-blue-500 hover:underline hover:underline-offset-2">
-                                <Addcart/>
-                            </span>
-                        </div>
-                        <div className="flex justify-between mt-5">
-                            <Points
-                            itemPrice={props.itemPrice}
-                            
                             />
-                            <Stock
-                            quantity={props.quantity}
-                            />
-                        </div>
+                        </h3>
+                        <span className="text-blue-500 hover:underline hover:underline-offset-2">
+                            <Addcart />
+                        </span>
                     </div>
-                </a>
-            </div>
-       
+                    <div className="flex justify-between mt-5">
+                        <Points
+                            itemPrice={props.itemPrice}
+                        />
+                        <Stock
+                            quantity={props.quantity}
+                        />
+                    </div>
+                </div>
+            </a>
+        </div>
+
     )
 }
 export default Itembox
