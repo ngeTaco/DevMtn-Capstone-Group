c50cdbdb47f221caaf61f490bcd9c4f4f5acf294
mcasto-09 <mgcastorani@gmail.com>
2024-10-01 21:34:24 -0600

    arrows are gone, notes added

--- src/components/storefront/CartDrawer.jsx
+++ src/components/storefront/CartDrawer.jsx
@@ -47,7 +47,9 @@ function CartDrawer() {
                         <li className="size-16 flex items-center space-x-4">
                             <div className="flex flex-col items-center">
                                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/poke-ball.png" alt="Pokeball" className="w-14 h-14" />
-                                <input type="number" placeholder="Quantity" className="mt-2 border rounded-md p-1 w-16 text-center" />
+                                <input type="number" id="minimum" min="1" placeholder="Quantity" className="mt-2 border rounded-md p-1 w-16 text-center" />
+                                {/* //remove arrows on input
+                                //add increment and decriment button(s) */}
                             </div>
                             <div className="flex flex-col text-lg">
                                 <p>Pokeball</p>
---
+++ src/store/storeReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    cartItems: []
+}
+
+export default function storeReducer(state = initialState, action) {
+    //console.log(action)
+    //console.log(state)
+
+    switch (action.type) {
+        default: return state
+    }
+}
