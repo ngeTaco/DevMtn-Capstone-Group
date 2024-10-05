c2ffef0eb5c75eaf3d61ea916daa5b7f66ca3fce
Aldo <thealdo.531@gmail.com>
2024-10-05 11:43:34 -0600

    navbar made, icon and shop name added to nav. basic nav from app jsx removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { Link, Outlet} from "react-router-dom"
+import { Link, Outlet } from "react-router-dom"
 import React from "react"
+import LogoutButton from "./components/NavBar/LogoutButton"
+import NavigationBar from "./components/NavBar/NavigationBar"
 
 //LANDING PAGE, SERVES AS HEADER KINDA, DOES NOT MOVE, WE CAN CHANGE THIS LATER
 
 function App() {
 
-    return (
-        <div>
-          <nav className="flex space-x-4 px-8">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/shop">Shop</Link>
-              </li>
-              <li>
-                <Link to="/">Login</Link>
-              </li>
-              <li>
-                <Link to="/profile">Profile</Link>
-              </li>
-              <li>
-                <Link to="/admin">Admin</Link>
-              </li>
-            </ul>
-          </nav>
-          <p></p>
-          <Outlet />
-        </div>
-        
-      )
-}
+  return (
+    <div>
+      <NavigationBar />
+      <Outlet />
+    </div>
 
+  )
+}
+//checkpoint is authenticate, update state to existing session
 export default App
---
+++ src/components/NavBar/LogoutButton.jsx
@@ -0,0 +1,11 @@
+export default function LogoutButton({ onLogout }) {
+
+    return (
+        <button 
+        type="submit"
+        class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+        >
+            Logout
+        </button>
+    )
+}
