910016b6d356b556ac23f010d8504b8ec869f82a
Aldo <thealdo.531@gmail.com>
2024-10-05 11:56:50 -0600

    styling and comment cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { Link, Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import React from "react"
-import LogoutButton from "./components/NavBar/LogoutButton"
 import NavigationBar from "./components/NavBar/NavigationBar"
-
-//LANDING PAGE, SERVES AS HEADER KINDA, DOES NOT MOVE, WE CAN CHANGE THIS LATER
 
 function App() {
 
--- src/components/NavBar/NavigationBar.jsx
+++ src/components/NavBar/NavigationBar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 export default function NavigationBar() {
 
     return (
-        <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+        <nav class="flex items-center justify-between flex-wrap bg-slate-700 p-6">
             <div class="flex items-center flex-shrink-0 text-white mr-7 scale-125">
                 <PokeballIcon />
                 <span class="font-semibold text-xl tracking-tight">PokéShop</span>
@@ -13,15 +13,15 @@ export default function NavigationBar() {
             <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                 <div class="text-sm lg:flex-grow">
                     <Link to="/shop"
-                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">
                         Shop
                     </Link>
                     <Link to="/profile"
-                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">
                         Profile
                     </Link>
                     <Link to="/admin"
-                        class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+                        class="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-sky-300 mr-4">
                         Admin
                     </Link>
                 </div>
