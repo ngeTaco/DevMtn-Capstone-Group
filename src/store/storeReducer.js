78164f47fc12568d4d699c94a84d5f2b6c008d93
Mary Castorani <mgcastorani@gmail.com>
2024-10-09 19:47:14 -0600

    mostly bug free, gg

--- src/store/storeReducer.js
+++ src/store/storeReducer.js
@@ -12,37 +12,6 @@ export default function storeReducer(state = initialState, action) {
                 cartItems: initialState.cartItems
             };
 
-        // case 'UPDATE_CART':
-        //     const itemToAdd = action.payload;
-        //     const itemToUpdate = state.cartItems.find(
-        //         (itemInCart) => itemInCart.id === itemToAdd.itemId
-        //     );
-        //     function createLineItem(item) {
-        //         return {
-        //             cartItemKey: item,
-        //             quantity: 1,
-        //             total: item.itemPrice,
-        //             id: item.itemId
-        //         };
-        //     }
-        //     if (itemToUpdate) {
-        //         return {
-        //             ...state,
-        //             cartItems: state.cartItems.map((item) => {
-        //                 if (item.id === itemToAdd.itemId) {
-        //                     item.quantity++;
-        //                     item.total = item.quantity * item.cartItemKey.itemPrice;
-        //                 }
-        //                 return item
-
-        //             })
-        //         }
-        //     } else {
-        //         return {
-        //             ...state,
-        //             cartItems: [...state.cartItems, createLineItem(itemToAdd)]
-        //         };
-        //     }
 
         case 'UPDATE_CART': {
             const itemToAdd = action.payload;
@@ -58,7 +27,6 @@ export default function storeReducer(state = initialState, action) {
                     id: item.itemId
                 };
             }
-
             if (itemToUpdate) {
                 return {
                     ...state,
@@ -81,24 +49,6 @@ export default function storeReducer(state = initialState, action) {
             }
         }
 
-
-        // case 'DECRIMENT_ITEM':
-        //     const itemToDecrement = state.cartItems.find(
-        //         (itemInCart) => itemInCart.id === action.payload.itemId
-        //     );
-        //     if (itemToDecrement) {
-        //         return {
-        //             ...state,
-        //             cartItems: state.cartItems.map((item) => {
-        //                 if (item.id === action.payload.itemId && item.quantity > 1) {
-        //                     item.quantity--;
-        //                     item.total = item.quantity * item.cartItemKey.itemPrice;
-        //                 }
-        //                 return item;
-        //             })
-        //         };
-        //     }
-        //     return state;
 
         case 'DECRIMENT_ITEM': {
             const itemToDecrement = state.cartItems.find(
